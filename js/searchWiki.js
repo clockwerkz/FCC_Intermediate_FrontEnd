b1219b9139ed9f20fbc3ad5d822f3a67eaca76e2
QbanCowboy <qbancowboy@qbancowboy.com>
2016-12-07 00:33:41 -0800

    API call is working now, just need to set it up correctly for the most optimal data to parse and display.

--- js/searchWiki.js
+++ js/searchWiki.js
@@ -1,15 +1,17 @@
 var searchValue;
-var wikiBase = "http://en.wikipedia.org/w/api.php?action=query&prop=revisions&rvprop=content&rvsection=0&titles=";
-var wikiJsonAttrib = "&format=json";
+var wikiBase = "https://en.wikipedia.org/w/api.php?action=query&prop=revisions&rvprop=content&rvsection=0&titles=";
+var wikiJsonAttrib = "&format=json&callback=?";
+
+console.log(document.getElementById("wikiReturn").innerHTML);
 
 function searchText() {
 	if (document.getElementById("searchTopic").value){
 		searchValue = document.getElementById("searchTopic").value;
 		$.getJSON(wikiBase+searchValue+wikiJsonAttrib, function(data){
-			document.getElementById("wikiReturn").innerHTML=data;
+			var jsonData = JSON.stringify(data);
+			document.getElementById("wikiReturn").innerHTML=jsonData;
 		});
 	} else {
 		text = "No data entered"
 	}
-	console.log(searchValue);
 }
