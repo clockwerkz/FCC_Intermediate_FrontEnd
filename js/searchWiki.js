db5a04011e2bd4b41fb48d9a0b2a6f037e077696
QbanCowboy <qbancowboy@qbancowboy.com>
2016-12-06 19:45:58 -0800

    WIP

--- js/searchWiki.js
+++ js/searchWiki.js
@@ -1,10 +1,15 @@
-var text;
+var searchValue;
+var wikiBase = "http://en.wikipedia.org/w/api.php?action=query&prop=revisions&rvprop=content&rvsection=0&titles=";
+var wikiJsonAttrib = "&format=json";
 
 function searchText() {
 	if (document.getElementById("searchTopic").value){
-		text = document.getElementById("searchTopic").value;
+		searchValue = document.getElementById("searchTopic").value;
+		$.getJSON(wikiBase+searchValue+wikiJsonAttrib, function(data){
+			document.getElementById("wikiReturn").innerHTML=data;
+		});
 	} else {
 		text = "No data entered"
 	}
-	console.log(text);
+	console.log(searchValue);
 }
