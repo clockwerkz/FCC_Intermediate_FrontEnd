e830cbfa5fb74e10eb7d6a7f6e265ab5ab12f940
QbanCowboy <qbancowboy@qbancowboy.com>
2016-12-08 08:09:07 -0800

    changed the wiki API string and it's starting to work now.  I'm only getting data from one page, but at least it's somewhat workable.  Now I just need to figure out how to call multiple pages.

--- js/searchWiki.js
+++ js/searchWiki.js
@@ -1,5 +1,6 @@
 var searchValue;
-var wikiBase = "https://en.wikipedia.org/w/api.php?action=query&prop=revisions&rvprop=content&rvsection=0&titles=";
+//var wikiBase = "https://en.wikipedia.org/w/api.php?action=query&prop=revisions&rvprop=content&titles=";
+var wikiBase = "https://en.wikipedia.org/w/api.php?action=parse&page=";
 var wikiJsonAttrib = "&format=json&callback=?";
 
 console.log(document.getElementById("wikiReturn").innerHTML);
@@ -7,10 +8,13 @@ console.log(document.getElementById("wikiReturn").innerHTML);
 function searchText() {
 	if (document.getElementById("searchTopic").value){
 		searchValue = document.getElementById("searchTopic").value;
+
 		$.getJSON(wikiBase+searchValue+wikiJsonAttrib, function(data){
 			var jsonData = JSON.stringify(data);
-			document.getElementById("wikiReturn").innerHTML=jsonData;
+			document.getElementById("wikiReturn").innerHTML=data.parse.text["*"];
+			console.log(jsonData);
 		});
+		
 	} else {
 		text = "No data entered"
 	}
