b9178a427a1527d0c6f2e1b72cfcd0cdac5d74a6
QbanCowboy <qbancowboy@qbancowboy.com>
2016-12-12 14:55:25 -0800

    Almost done with the project - search results now show up on the page properly formatted with the links to the search result working.  Random article button also has functionality.  The last TODO for me is adding form functionality.  Currently, if a user types in a topic and presses enter, nothing happens.  They have to click on the button to actually search.  The form is missing functionality, need to do some research.

--- js/searchWiki.js
+++ js/searchWiki.js
@@ -1,51 +1,71 @@
-var searchValue;
+var searchValue="";
 //var wikiBase = "https://en.wikipedia.org/w/api.php?action=query&prop=revisions&rvprop=content&titles=";
 var wikiBase = "https://en.wikipedia.org/w/api.php?action=parse&page=";
-var wikiJsonAttrib = "&format=json&callback=?";
 
 
-var wikiSearch = "https://en.wikipedia.org/w/api.php?action=query&list=search&srsearch=miami&prop=info&inprop=url&utf8=&format=jsonfm&callback=?"
+//Wikipedia result formatting
+var resultPanelHeading = '<div class="panel panel-default"><div class="panel-heading"><a href="';
+var resultPanelBody = '</h3></a></div><div class="panel-body">';
+var resultPanelFooter = '</div></div>';
+var resultPaneTitle = '" target="_blank"><h3 class="panel-title">';
+
+
+var wikiLinkBase = "https://en.wikipedia.org/wiki/";
+var whiteSpace = /\s+/g;
+
+var searchResults = document.getElementById("searchResults");
 var resultString = "";
+var urlLink = "";
 
-console.log(document.getElementById("wikiReturn").innerHTML);
+//var wikiSearch = "https://en.wikipedia.org/w/api.php?action=query&list=search&srsearch=miami&prop=info&inprop=url&utf8=&format=jsonfm&callback=?"
+
+
+function randomWiki() {
+	window.open("https://en.wikipedia.org/wiki/Special:Random");
+}
+
 
 function searchText() {
-	resultString = "";
-	if (document.getElementById("searchTopic").value){
-		searchValue = document.getElementById("searchTopic").value;
 
-		/*$.getJSON(wikiBase+searchValue+wikiJsonAttrib, function(data){
-		$.getJSON(wikiSearch, function(data){
-			var jsonData = JSON.stringify(data);
-			document.getElementById("wikiReturn").innerHTML=data;
-			console.log(jsonData);
-		});*/
-		$.ajax({
-		url:"https://en.wikipedia.org/w/api.php?action=query&list=search&srsearch=" + searchValue + "&prop=info&inprop=url&utf8=&format=json",
-		dataType: "jsonp",
-		async: false,
-		success: function (response) {
-			var obj = response.query;
-			//console.log(JSON.stringify(obj));
-			var items = obj.search;
-			itemList = Object.keys(items);
-				for (key in itemList){
-					resultString +="<h3>"+items[key].title+"</h3> <p>"+items[key].snippet+"</p><br>";
-				}
-				document.getElementById("wikiReturn").innerHTML=resultString;
-		},
-		error: function() {
-			alert("Error");
-		}
-	});
+	if (document.getElementById("searchTopic").value){
+
+		wikiSearch();
 		
 	} else {
-		text = "No data entered"
+		text = "No data entered";
 	}
 
-
-	
 }
+
+function wikiSearch() {
+	searchValue = document.getElementById("searchTopic").value;
+
+	$.ajax({
+			url:"https://en.wikipedia.org/w/api.php?action=query&list=search&srsearch=" + searchValue + "&prop=info&inprop=url&utf8=&format=json",
+			dataType: "jsonp",
+			async: false,
+			success: function (response) {
+
+				var obj = response.query;
+				var items = obj.search;
+
+				itemList = Object.keys(items);
+
+				for (key in itemList){
+				
+					resultString += resultPanelHeading + wikiLinkBase + items[key].title.replace(whiteSpace, "_") + resultPaneTitle + items[key].title+resultPanelBody + items[key].snippet + "..." + resultPanelFooter;	
+				}
+				searchResults.innerHTML = resultString;
+
+			},
+			error: function() {
+				alert("Error");
+			}
+		});
+}
+
+
+
 
 
 
@@ -56,4 +76,10 @@ https://en.wikipedia.org/w/api.php?action=query&list=search&srsearch=miami&forma
 
 https://en.wikipedia.org/w/api.php?action=query&list=search&srsearch=miami&formatversion=2
 
-*/
+/*resultDiv = document.createElement("div");
+					resultDiv.className += 'col-sm-8 col-sm-offset-2';
+					urlLink = items[key].title;
+					resultDiv.innerHTML = resultPanelHeading + wikiLinkBase + items[key].title.replace(whiteSpace, "_") + resultPaneTitle + items[key].title;
+					resultDiv.innerHTML +=resultPanelBody + items[key].snippet + "..." + resultPanelFooter;
+					wikiReturn.appendChild(resultDiv);*/	
+
